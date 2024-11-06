c2db71645f8bfb4372cb6289dadb8e88f8eb4876
Max <max@maxf.net>
2024-11-06 14:48:09 -0500

    updated

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,38 +11,39 @@ base {
 }
 
 plugins {
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+	// Apply the java-library plugin for API and implementation separation.
+	`java-library`
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+	// Use Maven Central for resolving dependencies.
+	mavenCentral()
+	mavenLocal()
 }
 
 dependencies {
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
+	// This dependency is exported to consumers, that is to say found on their compile classpath.
+	api(libs.commons.math3)
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
+	// This dependency is used internally, and not exposed to consumers on their own compile classpath.
+	implementation(libs.guava)
 
 	implementation("ch.qos.reload4j:reload4j:1.2.25")
 }
 
 testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use JUnit Jupiter test framework
-            useJUnitJupiter("5.10.2")
-        }
-    }
+	suites {
+		// Configure the built-in test suite
+		val test by getting(JvmTestSuite::class) {
+			// Use JUnit Jupiter test framework
+			useJUnitJupiter("5.10.2")
+		}
+	}
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
 }
